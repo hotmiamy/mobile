c8b389c666cb80bae98bfa6783f6b7c3d732c85f
llopes-n <mrllps07@gmail.com>
2025-10-21 20:05:48 -0300

    Exercise: register age

---
+++ facul/app/(tabs)/index.tsx
@@ -0,0 +1,27 @@
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Link href="/register" asChild>
+        <Button title="Registrar Idade" />
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
---
+++ facul/app/(tabs)/register.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+export default function RegisterScreen() {
+  const [idade, setIdade] = useState('');
+
+  const handleSave = () => {
+    if (!idade) {
+      alert('Por favor, insira sua idade.');
+      return;
+    }
+
+    alert(`Sua idade é ${idade} anos.`);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Digite sua idade:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder="Ex: 25"
+        value={idade}
+        onChangeText={setIdade}
+      />
+      <Button title="Salvar" onPress={handleSave} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#fff',
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#bbbbbbff',
+    width: '10%',
+    padding: 10,
+    borderRadius: 8,
+    textAlign: 'left',
+    marginBottom: 20,
+  },
+});
